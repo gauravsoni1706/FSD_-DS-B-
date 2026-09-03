0f73e525fdc5808920946a350e0a2b3dbc0609b3
Gaurav Soni <rajrajsoni000@gmail.com>
2026-09-03 10:09:17 +0530

    xyz

---
+++ JAVASCRIPT/promise.js
@@ -0,0 +1,27 @@
+function f1(){
+    return new Promise((resolve,reject)=>{
+         setTimeout(()=>{
+        console.log("hi!!!");
+        resolve();
+    },4000)
+
+    })
+    
+  
+}
+function f2(){
+     return new Promise((resolve,reject)=>{
+         setTimeout(()=>{
+        console.log("ABES COLLEGE");
+        resolve();
+    },2000)
+
+    })
+
+
+}
+f1().then(f2)
+       .catch((err)=>{
+        console.log("ERROR,err")
+       })
+f2();
