4d0ce9c2caad0a80eb20d88b985a8986a19f4c83
Gaurav Soni <rajrajsoni000@gmail.com>
2026-09-03 09:40:49 +0530

    arr

---
+++ JAVASCRIPT/callbackhell.js
@@ -0,0 +1,38 @@
+function test1(cb){
+     setTimeout(()=>{
+console.log("test1")
+     cb();
+     },4000);
+
+     
+}
+function test2(cb){
+      setTimeout(()=>{
+console.log("test2")
+      cb();
+     },2000);
+}
+function test3(cb){
+          setTimeout(()=>{
+console.log("test3")
+      cb();
+     },200);
+   
+}
+function test4(){
+            setTimeout(()=>{
+console.log("test4")
+      cb();
+     },200);
+   
+
+}
+
+test1(()=>{
+    test2(()=>{
+        test3(()=>{
+            test4();
+        })
+
+    });
+});
